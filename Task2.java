271f396fb9c6985e2234e78db34277049d366b9c
Andrey Zotin <andreyzotin95@gmail.com>
2022-09-08 21:55:32 +0300

    Task2 добавлена проверка на null

--- Task2.java
+++ Task2.java
@@ -6,16 +6,19 @@ public class Task2 {
     public static void main(String[] args) {
         int[] arr = {3, 4, 2, 7};
         int t = 10;
-        System.out.println(Arrays.toString(search(arr,t)));
+        System.out.println(Arrays.toString(search(arr, t)));
     }
 
-    public static int[] search(int[] numbers,int target) {
-
+    public static int[] search(int[] numbers, int target) {
+        if (numbers == null) {
+            return null;
+        }
         for (int i = 0; i < numbers.length; i++) {
             for (int j = 1; j < numbers.length; j++) {
                 if (numbers[i] + numbers[j] == target) {
                     return new int[]{numbers[i], numbers[j]};
                 }
+
             }
         }
         return numbers;
